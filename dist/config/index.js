cd0c6356dcfcd1b2179f39ba650b48453955d0aa
李跃祖 <liyuezu@souche.com>
2019-06-30 16:43:42 +0800

    feat: 初始化服务

---
+++ dist/config/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const systemConfig = {
+    port: 8000
+};
+exports.systemConfig = systemConfig;
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+// 第三方模块
+import bodyParser from 'body-parser';
+import express from 'express';
+import { NextFunction, Request, Response } from 'express'; // express 申明文件定义的类型
+
+// 自定义模块
+import { systemConfig } from './config';
+
+const app = express();
+
+// 处理 post 请求的请求体，限制大小最多为 20 兆
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
+app.use(bodyParser.json({ limit: '20mb' }));
+
+// error handler
+app.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
+  return res.sendStatus(500);
+});
+
+app.listen(systemConfig.port, function() {
+  console.log(`the server is start at port ${systemConfig.port}`);
+});
